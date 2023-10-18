7af7c6da5d08753538b2f8b8d1fcb12a2ae09fbb
Avontss <jorgemchan@gmail.com>
2023-10-18 11:20:02 -0300

    Correções deploy

--- src/main/java/com/blog/pessoal/BlogPessoalApplication.java
+++ src/main/java/com/blog/pessoal/BlogPessoalApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BlogPessoalApplication {
+public class BlogPessoalApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogPessoalApplication.class, args);
